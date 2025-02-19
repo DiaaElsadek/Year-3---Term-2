90516949e9dd2643505add7e7d82f7b7d2035a8c
Diaa Elsadek <diaadido1246@gmail.com>
2025-02-19 22:22:24 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ function downloadFile() {
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
-
     setTimeout(() => {
         window.open(fileURL, "_blank");
     }, 1000);
@@ -21,3 +20,16 @@ function goToPage(url) {
     window.open(url, '_blank');
     document.getElementById("popup").style.display = "none";
 }
+
+function toggleDiv(isVisible) {
+    document.getElementById("myDiv").style.display = isVisible ? "block" : "none";
+}
+
+
+setTimeout(function () {
+    console.log("Powerd By Diaa Elsadek !");
+    document.querySelectorAll(".subjects").forEach(subject => {
+        subject.style.animation = "none";
+    });
+}, 2000); // 3000 ميلي ثانية = 3 ثواني
+
